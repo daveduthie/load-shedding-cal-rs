75aac450cb465325a97e23621e7b1dac93a4a229
David Duthie <dave.a.duthie@gmail.com>
2023-04-24 21:46:01 +0200

    test: make ical.rs testable

--- rust_app/src/ical.rs
+++ rust_app/src/ical.rs
@@ -1,22 +1,27 @@
 use time::{format_description, macros::format_description, OffsetDateTime, UtcOffset};
 use uuid::Uuid;
 
-fn format_date_time(date_time: OffsetDateTime) -> String {
+fn fmt_dt(date_time: OffsetDateTime) -> String {
     const FMT: &[format_description::FormatItem] =
-        format_description!("[year][month][day]T[hour][second]Z");
+        format_description!("[year][month][day]T[hour][minute][second]Z");
     date_time.to_offset(UtcOffset::UTC).format(&FMT).unwrap()
 }
 
-pub fn event(start: OffsetDateTime, end: OffsetDateTime, title: &str) -> String {
-    let start_str = format_date_time(start);
-
-    let mut s = String::from("\nBEGIN:VEVENT");
-    s.push_str("\nUID:");
-    s.push_str(
+fn uid() -> String {
+    String::from(
         Uuid::new_v4()
             .as_hyphenated()
             .encode_lower(&mut Uuid::encode_buffer()),
-    );
+    )
+}
+
+fn event_(start: OffsetDateTime, end: OffsetDateTime, title: &str, uid: &str) -> String {
+    let start_str = fmt_dt(start);
+    let mut s = String::from("\nBEGIN:VEVENT");
+
+    s.push_str("\nUID:");
+    s.push_str(uid);
+
     s.push_str("\nDTSTAMP:");
     s.push_str(&start_str);
 
@@ -24,14 +29,17 @@ pub fn event(start: OffsetDateTime, end: OffsetDateTime, title: &str) -> String 
     s.push_str(&start_str);
 
     s.push_str("\nDTEND:");
-    s.push_str(&format_date_time(end));
+    s.push_str(&fmt_dt(end));
 
     s.push_str("\nSUMMARY:");
     s.push_str(title);
 
     s.push_str("\nEND:VEVENT");
-
     s
+}
+
+pub fn event(start: OffsetDateTime, end: OffsetDateTime, title: &str) -> String {
+    event_(start, end, title, &uid())
 }
 
 pub fn ical(events: &[String]) -> String {
@@ -46,32 +54,57 @@ pub fn ical(events: &[String]) -> String {
     s
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use time::macros::offset;
+#[cfg(test)]
+mod tests {
+    use time::Duration;
 
-//     use super::*;
+    use super::*;
 
-//     #[test]
-//     fn event_test() {
-//         assert_eq!(
-//             event(
-//                 OffsetDateTime::UNIX_EPOCH,
-//                 OffsetDateTime::now_utc(),
-//                 "hi there"
-//             ),
-//             "\nBEGIN:VEVENT\nUID:d61952fb-50ef-449b-9fc2-ec3927b23d8b\nDTSTAMP:19700101T0000Z\nDTSTART:19700101T0000Z\nDTEND:20230408T1836Z"
-//         )
-//     }
+    #[test]
+    fn event_test() {
+        let now = OffsetDateTime::now_utc();
+        let an_hours_time = now + Duration::HOUR;
+        assert_eq!(
+            event_(now, an_hours_time, "hi there", "id"),
+            format!(
+                r#"
+BEGIN:VEVENT
+UID:id
+DTSTAMP:{}
+DTSTART:{}
+DTEND:{}
+SUMMARY:hi there
+END:VEVENT"#,
+                fmt_dt(now),
+                fmt_dt(now),
+                fmt_dt(an_hours_time)
+            )
+        )
+    }
 
-//     #[test]
-//     fn ical_test() {
-//         let ic = ical(&vec![(
-//             OffsetDateTime::UNIX_EPOCH,
-//             OffsetDateTime::now_utc(),
-//             String::from("test event"),
-//         )]);
-//         // println!("{:#?}", &ic);
-//         assert_eq!(ic, String::from("foo"));
-//     }
-// }
+    #[test]
+    fn ical_test() {
+        let now = OffsetDateTime::now_utc();
+        let an_hours_time = now + Duration::HOUR;
+        let ic = ical(&[event_(now, an_hours_time, "test event", "id")]);
+        let expected = format!(
+            r#"BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//io.github.daveduthie//load-shedding-calendar//EN
+BEGIN:VEVENT
+UID:id
+DTSTAMP:{}
+DTSTART:{}
+DTEND:{}
+SUMMARY:test event
+END:VEVENT
+END:VCALENDAR"#,
+            fmt_dt(now),
+            fmt_dt(now),
+            fmt_dt(an_hours_time)
+        );
+        println!("{}", &ic);
+
+        assert_eq!(ic, expected);
+    }
+}
